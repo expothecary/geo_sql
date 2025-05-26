a34f488d3905c777437fbfdd9e13e81cdd23a42b
Aaron Seigo <aseigo@kde.org>
2025-05-26 22:55:01 +0200

    Add a file for PostGIS operators, start with &&

---
+++ lib/geo_sql/postgis/operators.ex
@@ -0,0 +1,13 @@
+defmodule GeoSQL.PostGIS.Operators do
+  @moduledoc """
+    Non-standard GIS operators found in PostGIS.
+
+    These are often more optimized and/or specialized than their ST_* equivalents.
+  """
+
+  defmacro bbox_intersects(geometryA, geometryB) do
+    quote do
+      fragment("? && ?", unquote(geometryA), unquote(geometryB))
+    end
+  end
+end

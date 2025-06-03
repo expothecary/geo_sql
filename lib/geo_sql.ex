9b92aec316e7c2420c12ce862786608fdf53c909
Aaron Seigo <aseigo@esri.com>
2025-06-03 14:17:35 +0200

    Add some module docs

--- lib/geo_sql.ex
+++ lib/geo_sql.ex
@@ -1,6 +1,15 @@
 defmodule GeoSQL do
   @moduledoc """
+  GeoSQL provides access to GIS functions in SQL databases. Currently supported are PostGIS 3.x and SpatialLite.
 
+  SQL functions are separated by their appearance in standards (e.g. SQL/MM2 vs SQL/MM3), category or topic
+  (such as MM3 topological functions) as well as their availability in specific implementations.
+
+  This makes usage of feature sets self-documenting in the code, allowing developers to adopt or avoid
+  calls that are (or are not) available in the databases in use.
+
+  Where there are subtle differences between implementations of the same functions, GeoSQL strives to
+  hide those differences behind single function calls.
   """
   def init(repo) do
     repo

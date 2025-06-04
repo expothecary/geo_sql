291f151e70ebfd945d50f59359ab357c5663f5c3
Aaron Seigo <aseigo@kde.org>
2025-06-04 09:39:27 +0200

    improve test table migration

--- priv/repo/migrations/20250603091627_initialize.exs
+++ priv/repo/migrations/20250603091627_initialize.exs
@@ -14,12 +14,32 @@ defmodule GeoSQL.PostGIS.Test.Repo.Migrations.Initialize do
 
     create table("geographiies") do
       add(:name, :text)
-      add(:geom, :geometry)
+      add(:geom, :geography)
+      add(:line, :geography)
     end
 
     create table("location_multi") do
       add(:name, :text)
       add(:geom, :geometry)
     end
+
+    execute(
+      "CREATE TABLE specified_columns
+        (
+          id int,
+          t  text,
+          point geometry(Point, 4326),
+          pointz geometry(PointZ, 4326),
+          linestring geometry(Linestring, 4326),
+          linestringz geometry(LineStringZ, 4326),
+          linestringzm geometry(LineStringZM, 4326),
+          polygon geometry(Polygon, 4326),
+          multipoint geometry(MultiPoint, 4326),
+          multilinestring geometry(MultiLinestring, 4326),
+          multipolygon geometry(MultiPolygon, 4326)
+        )
+      ",
+      "DROP TABLE specified_columns"
+    )
   end
 end

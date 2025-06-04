148e1b758d24452be3b086a6a43595764ff269ef
Aaron Seigo <aseigo@kde.org>
2025-06-04 17:07:57 +0200

    include all the PostGIS operators

--- lib/geo_sql/postgis/operators.ex
+++ lib/geo_sql/postgis/operators.ex
@@ -5,15 +5,51 @@ defmodule GeoSQL.PostGIS.Operators do
     These are often more optimized and/or specialized than their ST_* equivalents.
   """
 
+  defmacro bbox_equals?(geometryA, geometryB) do
+    quote do: fragment("? ~= ?", unquote(geometryA), unquote(geometryB))
+  end
+
   defmacro bbox_intersects?(geometryA, geometryB) do
     quote do: fragment("? && ?", unquote(geometryA), unquote(geometryB))
+  end
+
+  defmacro contained_by?(geometryA, geometryB) do
+    quote do: fragment("? @ ?", unquote(geometryA), unquote(geometryB))
+  end
+
+  defmacro contains?(geometryA, geometryB) do
+    quote do: fragment("? ~ ?", unquote(geometryA), unquote(geometryB))
+  end
+
+  defmacro equal?(geometryA, geometryB) do
+    quote do: fragment("? = ?", unquote(geometryA), unquote(geometryB))
   end
 
   defmacro nd_bbox_intersects?(geometryA, geometryB) do
     quote do: fragment("? &&& ?", unquote(geometryA), unquote(geometryB))
   end
 
-  defmacro overlaps_or_left?(geometryA, geometryB) do
-    quote do: fragment("? &&& ?", unquote(geometryA), unquote(geometryB))
+  defmacro overlaps_or_to_the?(direction, geometryA, geometryB) do
+    operator =
+      case direction do
+        :above -> "|&>"
+        :below -> "&<|"
+        :left -> "&<"
+        :right -> "&>"
+      end
+
+    quote do: fragment("? ? ?", unquote(geometryA), unquote(operator), unquote(geometryB))
+  end
+
+  defmacro strictly_to_the?(direction, geometryA, geometryB) do
+    operator =
+      case direction do
+        :above -> "|>>"
+        :below -> "<<|"
+        :left -> "<<"
+        :right -> ">>"
+      end
+
+    quote do: fragment("? ? ?", unquote(geometryA), unquote(operator), unquote(geometryB))
   end
 end

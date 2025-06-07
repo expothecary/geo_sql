78b1568f6040d1926aaa0bf0b12bbd5a086e9367
Aaron Seigo <aseigo@kde.org>
2025-06-07 17:07:58 +0200

    add support for a composition query for e.g. where clauses and a prefix. source is also a string

--- lib/geo_sql/postgis/vector_tiles/layer.ex
+++ lib/geo_sql/postgis/vector_tiles/layer.ex
@@ -1,12 +1,22 @@
 defmodule GeoSQL.PostGIS.VectorTiles.Layer do
   @enforce_keys [:name, :source, :columns]
-  defstruct [:name, :source, :columns]
+  defstruct [
+    :name,
+    :source,
+    :columns,
+    prefix: nil,
+    compose_query_fn: &__MODULE__.identity_composer/1
+  ]
 
   @type column_name :: atom
   @type columns_definition :: %{geometry: column_name, id: column_name, tags: column_name}
   @type t :: %__MODULE__{
           name: String.t(),
-          source: atom,
-          columns: columns_definition
+          source: String.t(),
+          prefix: String.t() | nil,
+          columns: columns_definition,
+          compose_query_fn: (Ecto.Query.t() -> Ecto.Query.t())
         }
+
+  def identity_composer(query), do: query
 end

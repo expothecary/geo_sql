a58bc59178d3b1d931aaf46237d3f9dd58d97639
Aaron Seigo <aseigo@kde.org>
2025-06-13 17:34:10 +0200

    add PostGIS.ThreeD.closest_point

--- lib/geo_sql/postgis/three_d.ex
+++ lib/geo_sql/postgis/three_d.ex
@@ -1,6 +1,10 @@
 defmodule GeoSQL.PostGIS.ThreeD do
   @moduledoc "Non-standard PostGIS 3D functions"
 
+  defmacro closest_point(geometryA, geometryB) do
+    quote do: fragment("ST_3DClosestPoint(?,?)", unquote(geometryA), unquote(geometryB))
+  end
+
   defmacro d_fully_within(geometryA, geometryB, distance) when is_number(distance) do
     quote do
       fragment(

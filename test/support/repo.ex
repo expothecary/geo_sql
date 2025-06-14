afc5ab46d5386842bd305a7085b210f1a92484b8
Aaron Seigo <aseigo@kde.org>
2025-06-14 16:49:30 +0200

    Call GeoSQL.init

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -2,3 +2,5 @@ defmodule GeoSQL.Test.PostGIS.Repo do
   use Ecto.Repo, otp_app: :geo_sql, adapter: Ecto.Adapters.Postgres
 end
 
+GeoSQL.init(GeoSQL.Test.PostGIS.Repo, json: Jason, decode_binary: :reference)
+

dcc9b80bcdc8a6cbdca2a3391c0740e7e94261e0
Aaron Seigo <aseigo@kde.org>
2025-06-14 18:24:34 +0200

    Add remaining non-standard Topo accessors

--- lib/geo_sql/common/topo.ex
+++ lib/geo_sql/common/topo.ex
@@ -7,7 +7,7 @@ defmodule GeoSQL.Common.Topo do
   """
 
   @spec get_edge_by_point(
-          topology :: String.t(),
+          object :: String.t(),
           topology_name :: String.t(),
           point :: GeoSQL.geometry_input(),
           tolerance :: number
@@ -27,7 +27,7 @@ defmodule GeoSQL.Common.Topo do
   end
 
   @spec get_face_by_point(
-          topology :: String.t(),
+          object :: String.t(),
           topology_name :: String.t(),
           point :: GeoSQL.geometry_input(),
           tolerance :: number
@@ -38,6 +38,60 @@ defmodule GeoSQL.Common.Topo do
     quote do
       fragment(
         "?.GetFaceeByPoint(?,?,?)",
+        unquote(object),
+        unquote(topology_name),
+        unquote(point),
+        unquote(tolerance)
+      )
+    end
+  end
+
+  @spec get_face_edges(
+          object :: String.t(),
+          topology_name :: String.t(),
+          face :: non_neg_integer()
+        ) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_face_edges(object, topology_name, face) do
+    quote do
+      fragment(
+        "?.ST_GetFaceEdges(?,?)",
+        unquote(object),
+        unquote(topology_name),
+        unquote(face)
+      )
+    end
+  end
+
+  @spec get_face_geometry(
+          object :: String.t(),
+          topology_name :: String.t(),
+          face :: non_neg_integer()
+        ) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_face_geometry(object, topology_name, face) do
+    quote do
+      fragment(
+        "?.ST_GetFaceGeometry(?,?)",
+        unquote(object),
+        unquote(topology_name),
+        unquote(face)
+      )
+    end
+  end
+
+  @spec get_node_by_point(
+          object :: String.t(),
+          topology_name :: String.t(),
+          point :: GeoSQL.geometry_input(),
+          tolerance :: number
+        ) ::
+          GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_node_by_point(object, topology_name, point, tolerance) do
+    quote do
+      fragment(
+        "?.GetNodeByPoint(?,?,?)",
         unquote(object),
         unquote(topology_name),
         unquote(point),
--- lib/geo_sql/postgis/topo.ex
+++ lib/geo_sql/postgis/topo.ex
@@ -7,19 +7,107 @@ defmodule GeoSQL.PostGIS.Topo do
   """
 
   @spec get_face_containing_point(
-          topology :: String.t(),
+          object :: String.t(),
           topology_name :: String.t(),
           point :: GeoSQL.geometry_input()
-        ) ::
-          GeoSQL.fragment()
+        ) :: GeoSQL.fragment()
   @doc group: "Accessors"
   defmacro get_face_containing_point(object, topology_name, point) do
     quote do
       fragment(
-        "?.GetFaceeContainingPoint(?,?,?)",
+        "?.GetFaceeContainingPoint(?,?)",
         unquote(object),
         unquote(topology_name),
         unquote(point)
+      )
+    end
+  end
+
+  @spec get_node_edges(
+          object :: String.t(),
+          topology_name :: String.t(),
+          node :: non_neg_integer()
+        ) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_node_edges(object, topology_name, node) do
+    quote do
+      fragment(
+        "?.GetNodeEdges(?,?)",
+        unquote(object),
+        unquote(topology_name),
+        unquote(node)
+      )
+    end
+  end
+
+  @spec get_ring_edges(
+          object :: String.t(),
+          topology_name :: String.t(),
+          ring :: non_neg_integer()
+        ) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_ring_edges(object, topology_name, ring) do
+    quote do
+      fragment(
+        "?.GetRingEdges(?,?)",
+        unquote(object),
+        unquote(topology_name),
+        unquote(ring)
+      )
+    end
+  end
+
+  @spec get_ring_edges(
+          object :: String.t(),
+          topology_name :: String.t(),
+          ring :: non_neg_integer(),
+          max_edges :: non_neg_integer()
+        ) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_ring_edges(object, topology_name, ring, max_edges) do
+    quote do
+      fragment(
+        "?.GetRingEdges(?,?,?)",
+        unquote(object),
+        unquote(topology_name),
+        unquote(ring),
+        unquote(max_edges)
+      )
+    end
+  end
+
+  @spec get_topology_id(object :: String.t(), topology_name :: String.t()) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_topology_id(object, topology_name) do
+    quote do
+      fragment(
+        "?.GetTopologyID(?)",
+        unquote(object),
+        unquote(topology_name)
+      )
+    end
+  end
+
+  @spec get_topology_id(object :: String.t(), topology_name :: String.t()) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_topology_srid(object, topology_name) do
+    quote do
+      fragment(
+        "?.GetTopologySRID(?)",
+        unquote(object),
+        unquote(topology_name)
+      )
+    end
+  end
+
+  @spec get_topology_name(object :: String.t(), topology_id :: number) :: GeoSQL.fragment()
+  @doc group: "Accessors"
+  defmacro get_topology_name(object, topology_id) do
+    quote do
+      fragment(
+        "?.GetTopologyName(?)",
+        unquote(object),
+        unquote(topology_id)
       )
     end
   end

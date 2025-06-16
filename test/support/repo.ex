92fa384f81da197f169ab48071fd2d9c7ee84830
Aaron Seigo <aseigo@kde.org>
2025-06-16 17:51:05 +0200

    move the ecto schemas to the test Repo
    a first step towards untangling the tests

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -2,5 +2,32 @@ defmodule GeoSQL.Test.PostGIS.Repo do
   use Ecto.Repo, otp_app: :geo_sql, adapter: Ecto.Adapters.Postgres
 end
 
+defmodule TestSchema.Location do
+  use Ecto.Schema
+
+  schema "locations" do
+    field(:name, :string)
+    field(:geom, GeoSQL.Geometry)
+  end
+end
+
+defmodule TestSchema.Geographies do
+  use Ecto.Schema
+
+  schema "geographies" do
+    field(:name, :string)
+    field(:geom, GeoSQL.Geometry)
+  end
+end
+
+defmodule TestSchema.LocationMulti do
+  use Ecto.Schema
+
+  schema "location_multi" do
+    field(:name, :string)
+    field(:geom, GeoSQL.Geometry)
+  end
+end
+
 # For the migrations.
 GeoSQL.init(GeoSQL.Test.PostGIS.Repo, json: Jason, decode_binary: :reference)

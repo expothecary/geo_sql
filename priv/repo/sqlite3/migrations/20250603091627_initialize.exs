cf2a4e5dd1bc1bf0399a54f1041e24ada7a23f3a
Aaron Seigo <aseigo@kde.org>
2025-06-20 13:32:17 +0200

    create all SQLite tables for tests

--- priv/repo/sqlite3/migrations/20250603091627_initialize.exs
+++ priv/repo/sqlite3/migrations/20250603091627_initialize.exs
@@ -2,42 +2,58 @@ defmodule GeoSQL.PostGIS.Test.Repo.Migrations.Initialize do
   use Ecto.Migration
 
   def change do
-#     execute("SELECT load_extension('mod_spatialite')")
     execute("SELECT InitSpatialMetadata()", "")
 
     create table("locations") do
       add(:name, :text)
-      add(:geom, :geometry)
     end
+
+    execute("SELECT AddGeometryColumn('locations', 'geom', 4326, 'GEOMETRY')", "")
 
     create table("geographies") do
       add(:name, :text)
-      add(:geom, :geography)
-      add(:line, :geography)
     end
+
+    execute("SELECT AddGeometryColumn('geographies', 'geom', 4326, 'GEOMETRY')", "")
+    execute("SELECT AddGeometryColumn('geographies', 'line', 4326, 'LINESTRING')", "")
 
     create table("location_multi") do
       add(:name, :text)
-      add(:geom, :geometry)
     end
 
-    #     execute(
-    #       "CREATE TABLE specified_columns
-    #         (
-    #           id int,
-    #           t  text,
-    #           point geometry(Point, 4326),
-    #           pointz geometry(PointZ, 4326),
-    #           linestring geometry(Linestring, 4326),
-    #           linestringz geometry(LineStringZ, 4326),
-    #           linestringzm geometry(LineStringZM, 4326),
-    #           polygon geometry(Polygon, 4326),
-    #           multipoint geometry(MultiPoint, 4326),
-    #           multilinestring geometry(MultiLinestring, 4326),
-    #           multipolygon geometry(MultiPolygon, 4326)
-    #         )
-    #       ",
-    #       "DROP TABLE specified_columns"
-    #     )
+    execute("SELECT AddGeometryColumn('location_multi', 'geom', 4326, 'GEOMETRY')", "")
+
+    create table("specified_columns") do
+      add(:t, :text)
+    end
+
+    execute("SELECT AddGeometryColumn('specified_columns', 'line', 4326, 'LINESTRING')", "")
+    execute("SELECT AddGeometryColumn('specified_columns', 'point', 4326, 'POINT')", "")
+    execute("SELECT AddGeometryColumn('specified_columns', 'pointz', 4326, 'POINTZ')", "")
+
+    execute("SELECT AddGeometryColumn('specified_columns', 'linestring', 4326, 'LINESTRING')", "")
+
+    execute(
+      "SELECT AddGeometryColumn('specified_columns', 'linestringz', 4326, 'LINESTRINGZ')",
+      ""
+    )
+
+    execute(
+      "SELECT AddGeometryColumn('specified_columns', 'linestringzm', 4326, 'LINESTRINGZM')",
+      ""
+    )
+
+    execute("SELECT AddGeometryColumn('specified_columns', 'polygon', 4326, 'POLYGON')", "")
+    execute("SELECT AddGeometryColumn('specified_columns', 'multipoint', 4326, 'MULTIPOINT')", "")
+
+    execute(
+      "SELECT AddGeometryColumn('specified_columns', 'multilinestring', 4326, 'MULTILINESTRING')",
+      ""
+    )
+
+    execute(
+      "SELECT AddGeometryColumn('specified_columns', 'multipolygon', 4326, 'MULTIPOLYGON')",
+      ""
+    )
   end
 end

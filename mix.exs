af6ff6983a5e886cb7acb97fc21556a89af12bd9
Aaron Seigo <aseigo@kde.org>
2025-07-02 01:08:05 +0200

    relax elixir version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule GeoSQL.Mixfile do
     [
       app: :geo_sql,
       version: @version,
-      elixir: "~> 1.18",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       name: "GeoSQL",
       deps: deps(),

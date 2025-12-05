54e2591f23dc3e7fe4ffdc3f3e6b1ee3028cec52
Aaron Seigo <aseigo@kde.org>
2025-12-05 12:13:26 +0100

    tests and fixes for ST_Expand

--- lib/geo_sql/postgis.ex
+++ lib/geo_sql/postgis.ex
@@ -202,8 +202,8 @@ defmodule GeoSQL.PostGIS do
 
   @spec expand(
           geometry :: GeoSQL.geometry_input(),
-          dx :: number,
-          dy :: number
+          dx :: GeoSQL.fragment() | number,
+          dy :: GeoSQL.fragment() | number
         ) :: GeoSQL.fragment()
   @doc group: "Bounding Boxes"
   defmacro expand(geometry, dx, dy) do
@@ -212,29 +212,29 @@ defmodule GeoSQL.PostGIS do
 
   @spec expand(
           geometry :: GeoSQL.geometry_input(),
-          dx :: number,
-          dy :: number,
-          dz :: number
+          dx :: GeoSQL.fragment() | number,
+          dy :: GeoSQL.fragment() | number,
+          dz :: GeoSQL.fragment() | number
         ) :: GeoSQL.fragment()
   @doc group: "Bounding Boxes"
   defmacro expand(geometry, dx, dy, dz) do
     quote do
-      fragment("ST_Expand(?,?,?)", unquote(geometry), unquote(dx), unquote(dy), unquote(dz))
+      fragment("ST_Expand(?,?,?, ?)", unquote(geometry), unquote(dx), unquote(dy), unquote(dz))
     end
   end
 
   @spec expand(
           geometry :: GeoSQL.geometry_input(),
-          dx :: number,
-          dy :: number,
-          dz :: number,
-          dm :: number
+          dx :: GeoSQL.fragment() | number,
+          dy :: GeoSQL.fragment() | number,
+          dz :: GeoSQL.fragment() | number,
+          dm :: GeoSQL.fragment() | number
         ) :: GeoSQL.fragment()
   @doc group: "Bounding Boxes"
   defmacro expand(geometry, dx, dy, dz, dm) do
     quote do
       fragment(
-        "ST_Expand(?,?,?)",
+        "ST_Expand(?,?,?,?,?)",
         unquote(geometry),
         unquote(dx),
         unquote(dy),
